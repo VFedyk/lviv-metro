4c2fc0d4ea24f4cd46781cdaf15870ffba542f80
Volodymyr Fedyk <volodymyr.fedyk@gmail.com>
2019-01-30 08:16:48 +0200

    chore: add fake Travis testing

--- test/ultimate-test-of-lviv.spec.js
+++ test/ultimate-test-of-lviv.spec.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 
 describe('Ultimate question of Lviv, the Universe and Everything:', () => {
-  it('Is metro built', () => {
+  it('metro should not be built', () => {
     const isMetroBuilt = false;
-    expect(isMetroBuilt, 'Metro is not built').to.be.true;
+    expect(isMetroBuilt, 'Metro is not built').to.be.false;
   });
 });
