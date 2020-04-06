db225a098537cff1884afc426775ba7daa0f95cd
Volodymyr Fedyk <volodymyr.fedyk@gmail.com>
2020-04-06 21:22:23 +0300

    fix: add corrections for getting news

--- bin/lviv-metro-news.js
+++ bin/lviv-metro-news.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 require('../src/commands/news');
--- src/utils/get-url.js
+++ src/utils/get-url.js
@@ -1,8 +1,17 @@
-const { readFileSync } = require('fs');
-const { get, globalAgent } = require('https');
-const { decode } = require('iconv-lite');
+const axios = require('axios');
+const { decodeStream } = require('iconv-lite');
 
-globalAgent.options.ca = readFileSync(__dirname + '/../../node_modules/node_extra_ca_certs_mozilla_bundle/ca_bundle/ca_intermediate_root_bundle.pem');
+const getDataFromStream = (dataStream, encoding) => {
+  return new Promise((resolve, reject) => {
+    dataStream.pipe(decodeStream(encoding)).collect((err, body) => {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(body)
+    })
+  })
+};
 
 /**
  * Get web-page content by HTTPS
@@ -10,13 +19,7 @@ globalAgent.options.ca = readFileSync(__dirname + '/../../node_modules/node_extr
  * @param {string} encoding
  * @returns {Promise<any>}
  */
-module.exports.getURL = (url, encoding = 'utf8') => {
-  return new Promise((resolve, reject) => {
-    get(url, (res) => {
-      let body = '';
-      res.on('data', data => body += decode(data, encoding));
-      res.on('error', (e) => reject(e));
-      res.on('end', () => resolve(body));
-    });
-  });
+module.exports.getURL = (url, encoding) => {
+  return axios({url, method: 'GET', responseType: 'stream'})
+    .then(({data}) => getDataFromStream(data, encoding))
 };
